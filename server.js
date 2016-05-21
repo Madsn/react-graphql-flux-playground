620eb438c141e8a7aa1db45b6de5fc9843890008
Madsn <m@madsn.net>
2016-05-21 18:30:58 +0200

    first commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 
 (async () => {
   try {
-    let db = await MongoClient.connect(process.env.MONGO_URL);
+    let db = await MongoClient.connect('mongodb://localhost');//process.env.MONGO_URL);
     let schema = Schema(db);
 
     app.use('/graphql', GraphQLHTTP({
